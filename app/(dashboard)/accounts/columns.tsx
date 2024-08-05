0758bc36207296f66d9483e0d128eec867fc5b52
Herthik <herthik.prathaban@outlook.com>
2024-08-05 14:18:22 +0800

    feat: accounts index page

---
+++ app/(dashboard)/accounts/columns.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+
+import { Checkbox } from '@/components/ui/checkbox';
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+   id: string;
+   amount: number;
+   status: 'pending' | 'processing' | 'success' | 'failed';
+   email: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+   {
+      id: 'select',
+      header: ({ table }) => (
+         <Checkbox
+            checked={
+               table.getIsAllPageRowsSelected() ||
+               (table.getIsSomePageRowsSelected() && 'indeterminate')
+            }
+            onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
+            aria-label="Select all"
+         />
+      ),
+      cell: ({ row }) => (
+         <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={value => row.toggleSelected(!!value)}
+            aria-label="Select row"
+         />
+      ),
+   },
+   {
+      accessorKey: 'status',
+      header: 'Status',
+   },
+   {
+      accessorKey: 'email',
+      header: 'Email',
+   },
+   {
+      accessorKey: 'amount',
+      header: 'Amount',
+   },
+];
---
+++ components/data-table.tsx
@@ -0,0 +1,130 @@
+'use client';
+
+import React from 'react';
+import {
+   ColumnDef,
+   flexRender,
+   getCoreRowModel,
+   getPaginationRowModel,
+   Row,
+   useReactTable,
+} from '@tanstack/react-table';
+import { Trash } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import {
+   Table,
+   TableBody,
+   TableCell,
+   TableHead,
+   TableHeader,
+   TableRow,
+} from '@/components/ui/table';
+
+interface DataTableProps<TData, TValue> {
+   columns: ColumnDef<TData, TValue>[];
+   data: TData[];
+   onDelete: (rows: Row<TData>[]) => void;
+   disabled?: boolean;
+}
+
+export function DataTable<TData, TValue>({
+   columns,
+   data,
+   onDelete,
+   disabled,
+}: DataTableProps<TData, TValue>) {
+   const [rowSelection, setRowSelection] = React.useState({});
+
+   const table = useReactTable({
+      data,
+      columns,
+      getCoreRowModel: getCoreRowModel(),
+      getPaginationRowModel: getPaginationRowModel(),
+      onRowSelectionChange: setRowSelection,
+      state: {
+         rowSelection,
+      },
+   });
+
+   return (
+      <div>
+         <div className="flex items-center py-4">
+            {table.getFilteredSelectedRowModel().rows.length > 0 && (
+               <Button
+                  disabled={disabled}
+                  size="sm"
+                  variant="outline"
+                  className="ml-auto font-normal text-xs"
+               >
+                  <Trash className="size-4 mr-2" />
+                  Delete ({table.getFilteredSelectedRowModel().rows.length})
+               </Button>
+            )}
+         </div>
+         <div className="rounded-md border">
+            <Table>
+               <TableHeader>
+                  {table.getHeaderGroups().map(headerGroup => (
+                     <TableRow key={headerGroup.id}>
+                        {headerGroup.headers.map(header => {
+                           return (
+                              <TableHead key={header.id}>
+                                 {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                         header.column.columnDef.header,
+                                         header.getContext()
+                                      )}
+                              </TableHead>
+                           );
+                        })}
+                     </TableRow>
+                  ))}
+               </TableHeader>
+               <TableBody>
+                  {table.getRowModel().rows?.length ? (
+                     table.getRowModel().rows.map(row => (
+                        <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                           {row.getVisibleCells().map(cell => (
+                              <TableCell key={cell.id}>
+                                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                              </TableCell>
+                           ))}
+                        </TableRow>
+                     ))
+                  ) : (
+                     <TableRow>
+                        <TableCell colSpan={columns.length} className="h-24 text-center">
+                           No results.
+                        </TableCell>
+                     </TableRow>
+                  )}
+               </TableBody>
+            </Table>
+         </div>
+         <div className="flex items-center justify-end space-x-2 py-4">
+            <div className="flex-1 text-sm text-muted-foreground">
+               {table.getFilteredSelectedRowModel().rows.length} of{' '}
+               {table.getFilteredRowModel().rows.length} row(s) selected.
+            </div>
+            <Button
+               variant="outline"
+               size="sm"
+               onClick={() => table.previousPage()}
+               disabled={!table.getCanPreviousPage()}
+            >
+               Previous
+            </Button>
+            <Button
+               variant="outline"
+               size="sm"
+               onClick={() => table.nextPage()}
+               disabled={!table.getCanNextPage()}
+            >
+               Next
+            </Button>
+         </div>
+      </div>
+   );
+}
