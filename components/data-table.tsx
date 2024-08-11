4355dd1617c4d9a4894ec9c3c4244542ba0598da
Herthik <herthik.prathaban@outlook.com>
2024-08-11 19:51:24 +0800

    feat: use confirm

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
    ColumnDef,
    flexRender,
@@ -11,6 +11,7 @@ import {
 } from '@tanstack/react-table';
 import { Trash } from 'lucide-react';
 
+import { useConfirm } from '@/hooks/use-confirm';
 import { Button } from '@/components/ui/button';
 import {
    Table,
@@ -34,6 +35,11 @@ export function DataTable<TData, TValue>({
    onDelete,
    disabled,
 }: DataTableProps<TData, TValue>) {
+   const [ConfirmDialog, confirm] = useConfirm(
+      'Are you sure?',
+      'Your are about to perform a bulk delete.'
+   );
+
    const [rowSelection, setRowSelection] = React.useState({});
 
    const table = useReactTable({
@@ -49,6 +55,7 @@ export function DataTable<TData, TValue>({
 
    return (
       <div>
+         <ConfirmDialog />
          <div className="flex items-center py-4">
             {table.getFilteredSelectedRowModel().rows.length > 0 && (
                <Button
@@ -56,9 +63,12 @@ export function DataTable<TData, TValue>({
                   size="sm"
                   variant="outline"
                   className="ml-auto font-normal text-xs"
-                  onClick={() => {
-                     onDelete(table.getFilteredSelectedRowModel().rows);
-                     table.resetRowSelection();
+                  onClick={async () => {
+                     const ok = await confirm();
+                     if (ok) {
+                        onDelete(table.getFilteredSelectedRowModel().rows);
+                        table.resetRowSelection();
+                     }
                   }}
                >
                   <Trash className="size-4 mr-2" />
---
+++ hooks/use-confirm.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+
+import {
+   AlertDialog,
+   AlertDialogAction,
+   AlertDialogCancel,
+   AlertDialogContent,
+   AlertDialogDescription,
+   AlertDialogFooter,
+   AlertDialogHeader,
+   AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+
+export const useConfirm = (
+   title: string,
+   message: string
+): [() => JSX.Element, () => Promise<unknown>] => {
+   const [promise, setPromise] = useState<{ resolve: (value: boolean) => void } | null>(null);
+
+   const confirm = () =>
+      new Promise((resolve, reject) => {
+         setPromise({ resolve });
+      });
+
+   const handleClose = () => {
+      setPromise(null);
+   };
+
+   const handleConfirm = () => {
+      promise?.resolve(true);
+      handleClose();
+   };
+
+   const handleCancel = () => {
+      promise?.resolve(false);
+      handleClose();
+   };
+
+   const ConfirmationDialog = () => (
+      <AlertDialog open={promise !== null}>
+         <AlertDialogContent>
+            <AlertDialogHeader>
+               <AlertDialogTitle>{title}</AlertDialogTitle>
+               <AlertDialogDescription>{message}</AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter className="pt-2">
+               <AlertDialogCancel onClick={handleCancel}>Cancel</AlertDialogCancel>
+               <AlertDialogAction onClick={handleConfirm}>Confirm</AlertDialogAction>
+            </AlertDialogFooter>
+         </AlertDialogContent>
+      </AlertDialog>
+   );
+
+   return [ConfirmationDialog, confirm];
+};
